6b4387951ed4539d3f3bac9d6092ff939113e2d9
GuilleFerres <guillermoferressanchez@gmail.com>
2024-06-18 20:22:04 +0200

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptatem minus ratione blanditiis adipisci exercitationem, quam maiores sequi. Harum, dolore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptatem minus ratione blanditiis adipisci exercitationem, quam maiores sequi. Harum, dolore.</p>
+    </div>
+   
+</template>
+<script lang="ts" setup>
+
+</script>
+<style lang="scss" scoped>
+    
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Product details for {{ id  }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe culpa alias at, neque praesentium dignissimos nam consequatur voluptate, modi et blanditiis ipsum dolores rem commodi maxime, incidunt atque iste dolorem.</p>
+    </div>
+</template>
+<script lang="ts" setup>
+const { id } = useRoute().params;
+definePageMeta({
+    layout: 'products'
+})
+</script>
+<style lang="scss" scoped>
+    
+</style>
